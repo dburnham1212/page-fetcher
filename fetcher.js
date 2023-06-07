2ecb2a7268f95f212e818810c4337678e71c2d81
dburnham1212 <dburnham1212@gmail.com>
2023-06-07 17:25:37 -0400

    adding use of user input to fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const request = require('request');
 
-const filePath = "./index.html"
-
 let fetchFile = () => {
   let args = process.argv.slice(2);
+  const url = args[0];
+  const filePath = args[1];
   if(args.length !== 2){
     throw new Error("Too many or too few arguments");
   }
-  request('https://www.example.edu', (_error, _response, body) => {
+  request(url, (_error, _response, body) => {
     fs.writeFile(filePath, body, err => {
       if (err) {
         console.error(err);
